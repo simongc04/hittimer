bbf79ebb04219ac32719b44304c97f500514840d
Navarro <joseanp0202@gmail.com>
2024-10-23 08:38:47 +0100

    Modificada clase CounterDown.kt

--- app/src/main/java/com/example/hiittimer/CounterDown.kt
+++ app/src/main/java/com/example/hiittimer/CounterDown.kt
@@ -2,36 +2,59 @@ package com.example.hiittimer
 
 import android.os.CountDownTimer
 
-class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
-    private var counterState : Boolean = false
+class CounterDown(initialTimeMillis: Long) {
+    private var countDownTime: Long = initialTimeMillis
+    private var timeRemaining: Long = initialTimeMillis
+    private var countDownTimer: CountDownTimer? = null
+    private var isPaused: Boolean = false
 
-    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
-
-        override fun onTick(millisUntilFinished: Long) {
-            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
+    fun start(onTick: (Long) -> Unit, onFinish: () -> Unit) {
+        if (countDownTimer == null) {
+            createCountDownTimer(timeRemaining, onTick, onFinish)
         }
+        countDownTimer?.start()
+        isPaused = false
+    }
 
-        override fun onFinish() {
-            counterState = false
+    fun pause() {
+        countDownTimer?.cancel()
+        isPaused = true
+    }
+
+    fun resume(onTick: (Long) -> Unit, onFinish: () -> Unit) {
+        if (isPaused) {
+            createCountDownTimer(timeRemaining, onTick, onFinish)
+            countDownTimer?.start()
+            isPaused = false
         }
     }
 
-    fun toggle() {
-        if (this.counterState){
-            this.cancel()
+    fun reset() {
+        countDownTimer?.cancel()
+        timeRemaining = countDownTime
+        countDownTimer = null
+        isPaused = false
+    }
 
-        } else {
-            this.start()
+    fun changeTime(newTimeMillis: Long) {
+        countDownTimer?.cancel()
+        countDownTime = newTimeMillis
+        timeRemaining = newTimeMillis
+        countDownTimer = null
+        isPaused = false
+    }
+
+    private fun createCountDownTimer(timeMillis: Long, onTick: (Long) -> Unit, onFinish: () -> Unit) {
+        countDownTimer = object : CountDownTimer(timeMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeRemaining = millisUntilFinished
+                onTick(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                timeRemaining = 0
+                onFinish()
+            }
         }
-    }
-
-    fun start() {
-        counterState = true
-        this.myCounter.start()
-    }
-
-    fun cancel() {
-        counterState = false
-        this.myCounter.cancel()
     }
 }
