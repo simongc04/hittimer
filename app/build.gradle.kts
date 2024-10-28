851b988adf4af92f0c9fe152fd6fd63ecf4fd9bc
Navarro <joseanp0202@gmail.com>
2024-10-28 11:43:52 +0000

    Añadida dependencia faltante de gson al proyecto para correcto funcionamiento con sharedpreferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
+    implementation(libs.gson)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
